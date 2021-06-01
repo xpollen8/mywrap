4c1a90e63cde2292da6678fda0f5790ec177972a
David Whittemore <git@xpollen8.com>
2021-06-01 11:33:23 -0700

    dw: mysqljs/mysql

--- index.js
+++ index.js
@@ -10,12 +10,12 @@ module.exports = class MyWrap {
 			database: '',
 			...config,
 		};
-		this.mysql = require('mysql2/promise');
+		this.mysql = require('mysqljs/mysql');
 	}
 	setDb = (db) => this.db = db;
 	getDb = () => this.db;
 	start = async (config) => {
-		this.setDb(await require('mysql2/promise').createPool({
+		this.setDb(await require('mysqljs/mysql').createPool({
 			...this.myConfig,
 			...config
 		}));
